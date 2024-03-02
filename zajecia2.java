09b67505b594328b16cc32a6a6c959d8a335f191
Dariusz Begiedza <dariusz.begiedza@gmail.com>
2024-03-02 10:11:59 +0100

    first commit

---
+++ zajecia2.java
@@ -0,0 +1,26 @@
+public class zajecia2 {
+
+  public static void main (String[] args){
+    // zad 1
+    int firstNumber = 13;
+    int secondNumber = 8;
+
+    System.out.println("Zadanie 1");
+
+    System.out.println(firstNumber + secondNumber);
+    System.out.println(firstNumber - secondNumber);
+    System.out.println(firstNumber * secondNumber);
+    System.out.println(firstNumber / secondNumber);
+    
+   
+   
+
+// zad 2
+    int[] numbers = {2,3,4,5,6};
+
+    System.out.println("Zadanie 2");
+  System.out.println(numbers / numbers.length)
+
+// zad 3
+  }
+}
